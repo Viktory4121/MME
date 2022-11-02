6d18df6916017ee271141da4a5339aa280491aaa
Viktory4121 <78976244+Viktory4121@users.noreply.github.com>
2022-11-02 19:37:37 +0300

    Создано с помощью Colaboratory

---
+++ test.r
@@ -0,0 +1,39 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMQNVsiUjkBQMNlzD2t1Tws",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "ir",
+      "display_name": "R"
+    },
+    "language_info": {
+      "name": "R"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Viktory4121/MME/blob/main/test.r\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "AUHocD6IMOgJ"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
